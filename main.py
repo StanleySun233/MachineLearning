0b95f2042e3877b33a0810b179d58c200a57db5f
sjsun <stanleysun233@163.com>
2022-11-16 16:38:21 +0800

    update: 更新第五次实验。

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-col = ["酒精", "苹果酸", "灰", "灰的碱性", "镁", "总酚", "类黄酮", "非黄烷类酚类", "花青素", "颜色强度", "色调", "稀释葡萄酒", "脯氨酸"]
-cls = ["琴酒", "雪莉", "贝尔摩斯"]
-X = pd.DataFrame(data=wine["data"], columns=col)
-Y = pd.DataFrame(data=wine["target"], columns=["标签"])
-df = pd.concat([X, Y], axis=1)
-df.head()
+iris = load_iris()
+
+X = iris["data"]
+Y = iris["target"]
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=RANDOM_STATUS)
